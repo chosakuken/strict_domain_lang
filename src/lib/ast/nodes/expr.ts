8edc4d5130d4bd1b822b0f05be0a2670cdc9954a
chosakuken <shunpc2018@outlook.jp>
2026-06-11 13:32:12 +0900

    :sparkles: feat: 比較演算の型追加 (#20)

--- src/lib/ast/nodes/expr.ts
+++ src/lib/ast/nodes/expr.ts
@@ -1,5 +1,6 @@
 import { BinaryNode } from "./binary.js";
+import { CompareNode } from "./compare.js";
 import { IntNode } from "./int.js";
 import { VarNode } from "./var.js";
 
-export type ExprNode = IntNode | BinaryNode | VarNode;
+export type ExprNode = IntNode | BinaryNode | VarNode | CompareNode;
