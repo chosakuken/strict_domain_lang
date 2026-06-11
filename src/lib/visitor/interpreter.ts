df648541c38eacc8b9513c7963e73dc157e6d55a
chosakuken <shunpc2018@outlook.jp>
2026-06-11 12:08:45 +0900

    :sparkles: feat: 変数関連のインタプリタ定義 (#13)

--- src/lib/visitor/interpreter.ts
+++ src/lib/visitor/interpreter.ts
@@ -5,16 +5,20 @@ import { IntValue } from "./runtimeValue/numberValue.js";
 import { BinaryNode } from "../ast/nodes/binary.js";
 import { IntNode } from "../ast/nodes/int.js";
 import { CallNode } from "../ast/nodes/call.js";
+import { AssignNode } from "../ast/nodes/assign.js";
+import { VarNode } from "../ast/nodes/var.js";
 
 type Func = (args: RuntimeValue[]) => RuntimeValue;
 
 export class BoqqiInterpreter implements Visitor<RuntimeValue> {
   private funcs: Map<string, Func>; // 関数表
+  private vars: Map<string, RuntimeValue>; // 変数表
   private readonly output: (text: string) => void; // 出力先
 
   constructor(private readonly outputDevice: (text: string) => void) {
     this.output = outputDevice;
     this.funcs = new Map<string, Func>();
+    this.vars = new Map<string, RuntimeValue>();
     // 組み込み関数
     this.funcs.set("print", (args: RuntimeValue[]) => {
       for (const arg of args) {
@@ -67,5 +71,17 @@ export class BoqqiInterpreter implements Visitor<RuntimeValue> {
     }
     // 実行
     return func(args);
+  }
+  visitAssign(node: AssignNode): RuntimeValue {
+    const value = node.expr.accept(this);
+    this.vars.set(node.name, value);
+    return value;
+  }
+  visitVar(node: VarNode): RuntimeValue {
+    const value = this.vars.get(node.name);
+    if (value === undefined) {
+      throw new Error(`変数 ${node.name} は未定義です`);
+    }
+    return value;
   }
 }
